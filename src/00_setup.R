21308a1d62f62c47ba7304d9b1d649c264e8986d
Federica Giua <federica.giua@studenti.unimi.it>
2020-01-16 12:43:19 +0100

    final setup

--- src/00_setup.R
+++ src/00_setup.R
@@ -1,4 +1,4 @@
-library(readr)
-dataset <- ("parlgov-stable.xlxs")
+library(readxl)
+data_2018 <- read_excel("data/original data/dataset_2018.xlsx")
 
-
+data_2015 <- read_excel("data/original data/dataset_2015.xlsx")

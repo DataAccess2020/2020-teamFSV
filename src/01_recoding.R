f2afb609d922680b990d52b7d7e7ee8baa3139db
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-16 17:18:36 +0100

    recode of the var "left_right"

--- src/01_recoding.R
+++ src/01_recoding.R
@@ -4,3 +4,7 @@ data_pg <- filter(
   country_name %in% ("Switzerland"), 
 )
 
+data_pg$pol_pos <- cut(data_pg$left_right, 
+                          breaks = c(0, 3.34, 6.67, 10), 
+                          labels = c("left", "center", "right" ),
+                          right = F)

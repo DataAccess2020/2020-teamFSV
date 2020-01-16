60e906b868e9578198ffad037d0f8ffe056efa69
Federica Giua <federica.giua@studenti.unimi.it>
2020-01-16 16:33:55 +0100

    new try

--- src/00_setup.R
+++ src/00_setup.R
@@ -1,4 +1,4 @@
-library(readxl)
-data_2018 <- read_excel("data/original data/dataset_2018.xlsx")
+library(readr)
+data <- read_csv("data/original data/view_election.csv")
 
-data_2015 <- read_excel("data/original data/dataset_2015.xlsx")
+

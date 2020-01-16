d04a29a22bf2f7290c602610b337558a7c563174
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-16 17:26:21 +0100

    deleting NA

--- src/01_recoding.R
+++ src/01_recoding.R
@@ -8,3 +8,4 @@ data_pg$pol_pos <- cut(data_pg$left_right,
                           breaks = c(0, 3.34, 6.67, 10), 
                           labels = c("left", "center", "right" ),
                           right = F)
+data_pg <- na.omit(data) 

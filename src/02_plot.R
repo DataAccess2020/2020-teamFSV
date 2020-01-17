541a1f47ac7c7e70c7c8253baf4ef37de4837853
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-17 18:25:42 +0100

    third (and final) plot

--- src/02_plot.R
+++ src/02_plot.R
@@ -2,7 +2,7 @@
 
 library(ggplot2)
 
-
+####PLOT 1: years (2019-2015-2011-2007)
 ggplot(data=data_pg, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   geom_bar(position = position_dodge(), stat="identity")+
   facet_wrap(~election_date)   +         
@@ -10,10 +10,10 @@ ggplot(data=data_pg, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 10)) +
   labs( x= "Political position",
         y = "Percentages of vote",
-        title = "Switzerland elections: 2019-2015-2011-2007",
+        title = "Switzerland elections: 2007-2011-2015-2019",
         col= "black")
 
-
+####PLOT 2: years (2003-1999-1995-1991)
 ggplot(data=data_2, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   geom_bar(position = position_dodge(), stat="identity")+
   facet_wrap(~election_date)   +         
@@ -21,7 +21,17 @@ ggplot(data=data_2, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 10)) +
   labs( x= "Political position",
         y = "Percentages of vote",
-        title = "Switzerland elections: 2003-1999-1995-1991",
+        title = "Switzerland elections: 1991-1995-1999-2003",
         col= "black")
 
 
+####PLOT 3: years (1975-1979-1983-1987)
+ggplot(data=data_3, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
+  geom_bar(position = position_dodge(), stat="identity")+
+  facet_wrap(~election_date)   +         
+  scale_fill_manual(values = c("red3", "seagreen", "steelblue4")) +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 10)) +
+  labs( x= "Political position",
+        y = "Percentages of vote",
+        title = "Switzerland elections: 1975-1979-1983-1987",
+        col= "black")

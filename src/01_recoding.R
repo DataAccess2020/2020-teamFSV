0a2328f00134f058361a1a68acbe1c81fe71b384
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-17 18:22:06 +0100

    third (and final) dataset

--- src/01_recoding.R
+++ src/01_recoding.R
@@ -1,5 +1,5 @@
 #this the recoding script
-
+####recoding 1
 library(dplyr)
 #here we are selecting only Switzerland observation and removing the missing values: 
 
@@ -29,6 +29,8 @@ data_pg <- filter(data_pg,
 library(rio)
 export(data_pg, "data_pg.dta")
 
+####recoding 2:
+
 #we have created a second dataset to check other years
 data_2 <-  filter(
   dat,
@@ -51,7 +53,30 @@ data_2$election_date <- as.Date(data_2$election_date)
 data_2 <- filter(data_2, 
                   election_date < "2007-10-21" & election_date >"1987-10-18")
 
+####recoding 3:
+####recoding 2:
 
+#we have created a second dataset to check other years
+data_3 <-  filter(
+  dat,
+  country_name == "Switzerland"
+)
+
+data_3$pol_pos <- cut(data_3$left_right, 
+                      breaks = c(0, 3.34, 6.67, 10), 
+                      labels = c("left", "center", "right" ),
+                      right = F)
+
+# Here we selected only the variables we needed for the analysis, to clean a bit the dataset: 
+data_3 <- select (data_3, country_name, election_date, vote_share, party_name_short, left_right, pol_pos)
+
+data_3 <- na.omit(data_3)
+
+data_3$election_date <- as.Date(data_3$election_date)
+
+#filtering only the years we are interested in: in this case 1987-1983-1979-1975
+data_3 <- filter(data_3, 
+                 election_date < "1991-10-20" & election_date >"1971-10-31")
 
 
 

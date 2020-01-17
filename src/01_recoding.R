b45d521fa378eda8792196cd9be0c53cf17f02c9
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-17 09:28:11 +0100

    cleaning of the dataset

--- src/01_recoding.R
+++ src/01_recoding.R
@@ -1,11 +1,19 @@
+#this the recoding script
+
 library(dplyr)
+#here we are selecting only Switzerland observation and removing the missing values: 
+
 data_pg <- filter(
   data,
-  country_name %in% ("Switzerland"), 
+  country_name == "Switzerland", 
+  na.rm==T
 )
 
+# Here we are recoding the new variable `pol_pos` in 3 categories (left, center and right): 
 data_pg$pol_pos <- cut(data_pg$left_right, 
                           breaks = c(0, 3.34, 6.67, 10), 
                           labels = c("left", "center", "right" ),
                           right = F)
-data_pg <- na.omit(data) 
+
+# Here we selected only the variables we needed for the analysis, to clean a bit the dataset: 
+data_pg <- select (data_pg, country_name, election_date, vote_share, party_name_short, left_right, pol_pos)

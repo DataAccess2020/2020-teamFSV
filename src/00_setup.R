bb039202a12f4023416a3efac8dfd3ac8c2bda1b
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-18 12:11:09 +0100

    comment inside the setup script

--- src/00_setup.R
+++ src/00_setup.R
@@ -1,3 +1,10 @@
-# this is the set-up for the analysis: we are loading the dataset. 
+#CE 1 
+#Team FSV
+
+#This script is the set up for the data analysis of the project:
+
+# load the package----------------------------------------------
 library(readr)
+
+# importing the original dataset: ------------------------------
 dat <- read_csv("data/original data/view_election.csv")

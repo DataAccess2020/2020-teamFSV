ae064b4be5395b196114ab1ea97ca445fb7ecffd
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-01-18 12:26:15 +0100

    change in the comments

--- src/02_plot.R
+++ src/02_plot.R
@@ -1,8 +1,19 @@
-# plotting vote_share and pol_pos in the 4 years selected: 
+#CE 1 
+#Team FSV
 
+
+# This script include the creation of the plots: 
+#- plot 1: years (2007-2011-2015-2019)
+#- plot 2: years (1991-1995-1999-2003)
+#- plot 3: years (1975-1979-1983-1987)
+
+#Creating the plots:-------------------------------------------
+
+# load the package:
 library(ggplot2)
 
-####PLOT 1: years (2019-2015-2011-2007)
+#PLOT 1: years (2007-2011-2015-2019)
+
 ggplot(data=data_pg, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   geom_bar(position = position_dodge(), stat="identity")+
   facet_wrap(~election_date)   +         
@@ -13,7 +24,8 @@ ggplot(data=data_pg, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
         title = "Switzerland elections: 2007-2011-2015-2019",
         col= "black")
 
-####PLOT 2: years (2003-1999-1995-1991)
+# PLOT 2: years (1991-1995-1999-2003)
+
 ggplot(data=data_2, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   geom_bar(position = position_dodge(), stat="identity")+
   facet_wrap(~election_date)   +         
@@ -24,8 +36,8 @@ ggplot(data=data_2, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
         title = "Switzerland elections: 1991-1995-1999-2003",
         col= "black")
 
+#PLOT 3: years (1975-1979-1983-1987)
 
-####PLOT 3: years (1975-1979-1983-1987)
 ggplot(data=data_3, aes(x=pol_pos, y=vote_share, fill=pol_pos)) +
   geom_bar(position = position_dodge(), stat="identity")+
   facet_wrap(~election_date)   +         
